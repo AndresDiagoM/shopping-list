2be2c06946f7093b79c9d32bc58196202814aeb4
Andres Diago <andresdiag@unicauca.edu.co>
2023-05-21 13:53:49 -0500

    :sparkles: Added new components.
    Added img, products components.

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product, updateProductDTO, createProductDTO } from '../models/product.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  // --------Propiedades--------
+  ApiFirebase = 'https://shopping-list-c69d4-default-rtdb.firebaseio.com';
+
+  // --------CONSTRUCTOR--------
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // --------METODOS--------
+  getProducts() {
+    return this.http.get<Product[]>(`${this.ApiFirebase}/productos.json`);
+  }
+  getProduct(id: string) {
+    return this.http.get<Product>(`${this.ApiFirebase}/${id}.json`);
+  }
+  create(product: createProductDTO | Product) {
+    // add product with a post request with incremental
+    return this.http.post<Product>(this.ApiFirebase, product);
+  }
+  delete(product: Product) {
+    return this.http.delete<Product>(`${this.ApiFirebase}/${product.id}${'.json'}`);
+  }
+  update(id: string, dto: updateProductDTO){
+    return this.http.put<Product>(`${this.ApiFirebase}/${id}${'.json'}`, dto);
+  }
 }
